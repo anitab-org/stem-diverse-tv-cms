f9ad90b3d1d8a503f599e2a6779f591fe3dfcd5d
annabauza <31966073+annabauza@users.noreply.github.com>
2020-08-15 18:53:44 +0100

    feat: init react web

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to STEM Diverse TV CMS.</p>
+        <p>Under Construction</p>
+        <a
+          className="App-link"
+          href="https://github.com/anitab-org/stem-diverse-tv-cms"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn More
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
