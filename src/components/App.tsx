ce497ed8411474bc12144f379c2b0900d6e28f4b
chinmaym07 <b418020@iiit-bh.ac.in>
2021-02-23 15:39:03 +0530

    Fixed : Registeration & Login on the Hosted App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -35,7 +35,7 @@ const routes = [
 
 function App() {
     const { siteWrapper } = useStyles();
-
+    
     return (
         <Router>
             <AuthProvider>
--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
+// Locally you need to change the API_URL to http://localhost:5000
+// When you want to register your account on the hosted backend use API_URL = https://stem-diverse-tv.herokuapp.com/
 
-// const API_URL = 'http://localhost:5000'
-const API_URL = ''; // we are proxying the requests to the backend
+const API_URL_PROD = 'https://stem-diverse-tv.herokuapp.com/'; 
+const API_URL_LOCAL = 'http://localhost:5000'; 
 
 const publicFetch = axios.create({
-    baseURL: API_URL,
+    baseURL: process.env.REACT_APP_ENVIRONMENT_CONFIG === "local"?API_URL_LOCAL: API_URL_PROD,
 });
 
 export { publicFetch };
